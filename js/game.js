90aa8afd6ad8c065bc736999e99f95ae32fc0e0f
cassiemak <cassie.mak@gmail.com>
2014-12-02 15:52:29 +0800

    timer works

--- js/game.js
+++ js/game.js
@@ -1,7 +1,58 @@
 $(document).ready(function(){
+  'use strict';
+  var timeLeft = 10;
 
-  // Addition function
 
-  
+  var addition = function(num1, num2){
+    var sum = num1 + num2;
+    return sum;
+  };
 
+  var subtraction = function(num1, num2){
+    var minus = num1 - num2;
+    return minus;
+  };
+
+  var multiplication = function (num1, num2){
+    var multiple = num1 * num2;
+    return multiple;
+  };
+
+  var division = function (num1, num2){
+    var divide = num1 / num2;
+  };
+
+// function that generate the questions
+  var generateQ = function(){
+    $(".firstNum") = math.random();
+    var number1 = $(".firstNum");
+    $(".secondNUm") = math.random();
+    var number2 = $(".secondNUm");
+
+  }
+
+// set a function that countdown every second, 
+  var countdown10 = function(){
+    setInterval(decrementTimer, 1000);
+  };
+  function decrementTimer (){
+    // console.log(timeLeft);
+    $(".timer").text(timeLeft);
+    timeLeft--;
+  }
+
+// click button to activate function
+  $(".play").click(function(){
+    countdown10();
+    // console.log("OK!");
+  })
+
+  $(function(){
+      $("#slideBar").slider();
+  });
+
+  // 
+
+
+// Closing.ready function
 });
